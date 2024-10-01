05c80361335e53554ed7b347351a100098d0d89c
Sebas Monje <sebasmonje12@gmail.com>
2024-10-01 19:54:12 -0400

    Cambios para Obtener JEDECID de la memoria Flash
    Se modifico el archivo hardware_pins, y se agregó la librería Adafruit_SPIFlash, modificando algunos archivos en el para utilizar la memoria W25Q128JV_SM en modo QSPI. Se tomó como ejemplo el archivo variant de la placa de desarrollo de Adafruit Metro M4 que utiliza el SAMD51

--- include/hardware_pins.h
+++ include/hardware_pins.h
@@ -17,12 +17,17 @@
 #define SPI_SCK_MAX     PIN_SPI_SCK         // PA05
 #define Interface_EN    D11                 // PB15
 
-#define QSPI_D0         PIN_SPI1_SCK        // PA08     Revisar si se puede QSPI
-#define QSPI_D1         PIN_SPI1_MOSI       // PA09
-#define QSPI_D2         PIN_SPI1_MISO       // PA10
-#define QSPI_D3         PIN_SPI1_SS         // PA11
-#define QSPI_CS         G7                  // PB11     Ruteado a CS de la memoria flash
-#define QSPI_SCK        G6                  // PB10     Ruteado a SCK de la memoria flash
+// #define QSPI_D0         PIN_SPI1_SCK        // PA08     Revisar si se puede QSPI
+// #define QSPI_D1         PIN_SPI1_MOSI       // PA09
+// #define QSPI_D2         PIN_SPI1_MISO       // PA10
+// #define QSPI_D3         PIN_SPI1_SS         // PA11
+
+#define QSPI_D0         PIN_QSPI_D0         // PA08     Revisar si se puede QSPI
+#define QSPI_D1         PIN_QSPI_D1         // PA09
+#define QSPI_D2         PIN_QSPI_D2         // PA10
+#define QSPI_D3         PIN_QSPI_D3         // PA11
+#define QSPI_CS         G5                  // PB11     Ruteado a CS de la memoria flash
+#define QSPI_SCK        G4                  // PB10     Ruteado a SCK de la memoria flash
 #define UART_BOSS_rx    PIN_SERIAL1_RX      // PB30
 #define UART_BOSS_tx    PIN_SERIAL1_TX      // PB31
 #define UART_GPS_rx     PIN_WIRE1_SDA       // PA13
@@ -31,8 +36,8 @@
 #define USB_MUA_P       PIN_USB_PM          // PA25
 
 /////// MICROMOD
-#define PA14        D10
-#define PA15        D11
+#define PA14        38
+#define PA15        39
 #define PA18        I2CINT
 #define PA19        14
 #define PA20        PIN_I2S_FS
--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,57 @@
 #include <Arduino.h>
 #include <Adafruit_SPIFlash.h>
-#include <Adafruit_FlashTransport_QSPI.h>
 
+
+
+// #include "flash_config"
 #include "hardware_pins.h"
 
-Adafruit_FlashTransport_QSPI Flash_QSPI;
-// Adafruit_SPIFlash flash(&Flash_QSPI);
+Adafruit_FlashTransport_QSPI flashTransport(QSPI_SCK, QSPI_CS, QSPI_D0,
+                                   QSPI_D1, QSPI_D2, QSPI_D3);
+Adafruit_SPIFlash Flash_QSPI(&flashTransport);
 
 #define P PB08
 
 void setup() {
 
-  delay(4000);
+  delay(3000);
 
   Serial.begin(115200);
   Serial.println("Iniciado.");
 
+  pinMode(PB08, OUTPUT);
+  // pinMode(QSPI_CS, OUTPUT); // Pin con resistencia pullup
+  // delay(500);
+  // digitalWrite(QSPI_CS, LOW);
 
-  pinMode(P, OUTPUT);
+  // Inicializar el flash QSPI
+  flashTransport.begin();
+  if (flashTransport.supportQuadMode()) {
+    Serial.println("Soporta QSPI");
+  } else {
+      Serial.println("No soporta QSPI");
+  }
+  
+  // Flash_QSPI.begin();
+  while (Flash_QSPI.begin() == false) {
+    Serial.println("Error al inicializar la memoria");
+    delay(1000);  // Detener si falla la inicialización
+  }
+  if (Flash_QSPI.isReady()) {
+    Serial.println("La mem flash Está lista");
+    Serial.print("ID JEDEC de la memoria flash: 0x");
+    Serial.println(Flash_QSPI.getJEDECID(), HEX);  // Mostrar el ID en formato hexadecimal
+  }
 
+  Serial.println("Memoria flash_QSPI inicializada correctamente");
 
 }
 
 void loop() {  
-
+  // digitalWrite(QSPI_CS, LOW);
+  digitalWrite(PB08, LOW);
+  delay(1000);
+  digitalWrite(PB08, HIGH);
+  // digitalWrite(QSPI_CS, HIGH);
+  delay(1000);
 }
